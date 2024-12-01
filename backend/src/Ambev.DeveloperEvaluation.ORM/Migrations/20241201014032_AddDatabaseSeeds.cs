f09b73a410050de1842c329d55e9210aa1cdc517
Thiago de Souza Rios <thiago.rios@glpg.tech>
2024-11-30 23:15:05 -0300

    chore: include migration seed to fill tables when initialize application

---
+++ backend/src/Ambev.DeveloperEvaluation.ORM/Migrations/20241201014032_AddDatabaseSeeds.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ambev.DeveloperEvaluation.ORM.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDatabaseSeeds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Branchs",
+                columns: new[] { "Name" },
+                values: new object[,]
+                {
+                    { "Filial 1" },
+                    { "Filial 2" }
+                }
+            );
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Name", "UnitPrice" },
+                values: new object[,]
+                {
+                    { "Beer A", 5.90 },
+                    { "Beer B", 10 }
+                }
+            );
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "Name", "Document" },
+                values: new object[,]
+                {
+                    { "Client 1", "00011122233" },
+                    { "Cliente 2", "44455566677" }
+                }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {            
+        }
+    }
+}
